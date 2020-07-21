1c2f2a9849d76086f51441e25572cc8d64a1a26c
booger79 <yamtar65@gmail.com>
2020-07-21 19:06:31 +0300

    hello world
    added codes

---
+++ Data/Entities/College.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class College:BaseEntity
+    {
+        public string City { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ Data/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid FieldId { get; set; }
+        public Guid CollegeId { get; set; }
+
+    }
+}
